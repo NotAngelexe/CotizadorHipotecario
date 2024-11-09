6600c8b38837fe762a320f2bb71ed5dba367780c
NeriU <118403833+NeriU@users.noreply.github.com>
2024-11-08 23:11:44 -0600

    clientes v1

--- backCotizaciones/controllers/clientesController.js
+++ backCotizaciones/controllers/clientesController.js
@@ -4,13 +4,78 @@ class ClienteController{
 
   }
   consultar(req,res){
-    res.json({ msg: 'Clientes' });
+    try{
+      db.query(
+        `SELECT * FROM cotizaciones.cliente`,
+        (err, rows) => {
+          if (err) {
+            res.status(400).json({ error: error.sqlMessage });
+          }
+          res.status(200).json(rows);
+        }
+      );
+    }catch(err){
+      res.status(500).json({ error: err.message });
+    };
   }
   consultaByID(req,res){
+    try{
+      const{id}=req.params;
+      db.query(
+        `SELECT * FROM cotizaciones.cliente WHERE id_cliente=?`,[id],
+        (err, rows) => {
+          if (err) {
+            res.status(400).json({ error: err.sqlMessage });
+          }
+          res.status(200).json(rows);
+        }
+      );
+    }catch(err){
+      res.status(500).json({ error: err.message });
+    };
   }
   modificar(req,res){
+    const{id}=req.params;
+    try {
+      const { nombre, rfc, edad, sueldo, telefono, correo, password } = req.body;
+  
+      db.query(
+        `UPDATE cotizaciones.cliente 
+          SET nombre=?,rfc=?,edad=?,sueldo=?,fecha_modificacion=NOW(), telefono=?, correo=?,password=? WHERE id_cliente=?;`,
+        [nombre, rfc, edad, sueldo, telefono, correo, password,id],
+        (err, rows) => {
+          if (err) {
+            res.status(400).json({ error: error.sqlMessage });
+          }
+
+          if(rows.affectedRows==1){
+            res.status(200).json({message:"Informacion de cliente actualizada"});
+          }
+
+        }
+      );
+  
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
   }
   baja(req,res){
+    try{
+      const{id}=req.params;
+      db.query(
+        `DELETE FROM cotizaciones.cliente WHERE id_cliente=?`,[id],
+        (err, rows) => {
+          if (err) {
+            res.status(400).json({ error: err.sqlMessage });
+          }
+          if(rows.affectedRows==1){
+            res.status(200).json({message:"Cliente Eliminado"});
+          }
+        }
+      );
+    }catch(err){
+      res.status(500).json({ error: err.message });
+    };
   }
   alta(req, res) {
     try {
@@ -21,14 +86,11 @@ class ClienteController{
           (nombre, rfc, edad, sueldo, fecha_alta, fecha_baja, fecha_modificacion, telefono, correo, password) 
          VALUES (?, ?, ?, ?, NOW(), ?, NOW(), ?, ?, ?);`,
         [nombre, rfc, edad, sueldo, fecha_baja, telefono, correo, password],
-        (error, results) => {
-          if (error) {
-            console.error("SQL Error: ", error.sqlMessage);
-            res.status(500).json({ error: error.sqlMessage });
-          } else {
-            console.log("Record inserted successfully");
-            res.status(201).json({ message: "Record inserted successfully", results });
+        (err, rows) => {
+          if (err) {
+            res.status(400).json({ error: error.sqlMessage });
           }
+          res.status(201).json({id:rows.insertId});
         }
       );
   
